82be89b377edd1c8f06ab3a31eac35fb646dc588
Pulipuli Chen <pulipuli.chen@gmail.com>
2026-05-27 23:55:42 +0800

    Implement initial project structure and setup

---
+++ 1.Host/1.environment_install.sh
@@ -0,0 +1,4 @@
+# 確認環境有安裝 rclone, rsync
+
+# 確認rclone 有 target (寫在 .env 中)
+
---
+++ 2.Drive/drive-update.sh
@@ -0,0 +1,9 @@
+# 先建立臨時目錄，讓它能夠掛載 rclone target
+
+# 使用 rclone mount 掛載 rclone target
+
+# 使用 rsync 同步資料，注意 exclude 的目錄名稱開頭字串，寫在 .env 中
+
+# 卸載 rclone mount
+
+# 刪除臨時目錄
